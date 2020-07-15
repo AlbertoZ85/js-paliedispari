8a18e0ebeca5da733cc5f7f7a0a8f6bfbe62eefb
Alberto Zordan <zorberto@gmail.com>
2020-07-15 15:19:26 +0200

    Some minor tweaking...

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ var utente = prompt('Inserisci una parola');
 var risultato = 'Mmm... scelta banale';
 var bgc = "#e60000";
 
-// 2. Istruzione condizionale in caso di riscontro positivo, invocando la funzione palindroma
-if (utente == palindroma(utente)) {
+// 2. Istruzione condizionale in caso di riscontro positivo, invocando la funzione 'inversa'
+if (utente == inversa(utente)) {
     risultato = 'Bella lì, hai beccato una palindroma!';
     bgc = "lightgreen";
 }
@@ -16,8 +16,8 @@ if (utente == palindroma(utente)) {
 document.getElementById('esito').innerHTML = risultato;
 document.getElementById('esito').style.backgroundColor = bgc;
 
-// Creo una funzione che legge una parola di input e ne produce la versione palindroma nel seguente modo: un ciclo for salva in una variabile i caratteri della parola intesi come elementi di un array (stringa), letti a partire dalla fine, concatenandoli per creare la stringa palindroma
-function palindroma(parola) {
+// Creo una funzione che legge una parola di input e ne produce la versione inversa (scritta al contrario) nel seguente modo: un ciclo for salva in una variabile i caratteri della parola intesi come elementi di un array (stringa), letti a partire dalla fine, concatenandoli per creare la stringa inversa
+function inversa(parola) {
     var reverse = '';
     for (var i = parola.length - 1; i >= 0; i--) {
         reverse += parola[i];
